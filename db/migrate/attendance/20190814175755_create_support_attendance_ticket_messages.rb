87fddeeac5476c54595714a42e88e7416b7a78fc
Lucas Garrido <lucaspg.j@gmail.com>
2019-08-14 15:20:50 -0300

    Criando Migrations do atendimentos

--- db/migrate/attendance/20190814175755_create_support_attendance_ticket_messages.rb
+++ db/migrate/attendance/20190814175755_create_support_attendance_ticket_messages.rb
@@ -1,6 +1,11 @@
 class CreateSupportAttendanceTicketMessages < ActiveRecord::Migration[5.2]
   def change
-    create_table :support_attendance_ticket_messages do |t|
+    create_table 'sihabx.attendance_ticket_messages' do |t|
+
+      t.integer :ticket_id
+      t.integer :attendant_id
+      t.text    :message 
+      t.boolean :cadastre, default: false
 
       t.timestamps
     end
