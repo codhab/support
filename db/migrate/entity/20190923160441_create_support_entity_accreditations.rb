2e7aacc34b20718d9f1ad2ff0867e7f777d97b13
Elton Silva <elton.chrls@gmail.com>
2019-09-24 11:06:47 -0300

    Correção de require_dependency

--- db/migrate/entity/20190923160441_create_support_entity_accreditations.rb
+++ db/migrate/entity/20190923160441_create_support_entity_accreditations.rb
@@ -2,7 +2,9 @@ class CreateSupportEntityAccreditations < ActiveRecord::Migration[5.2]
   def change
     create_table 'sihabx.entity_accreditations' do |t|
       t.string  :name      
-      t.string  :process_number      
+      t.string  :process_number  
+      t.date    :started_at  
+      t.date    :ended_at  
       t.text    :observation      
       t.boolean :status, default: true      
 
