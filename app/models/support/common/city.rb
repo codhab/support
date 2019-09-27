16303285b3905e0d1a0c28b071439a1a9a39bbe5
Elton Silva <elton.chrls@gmail.com>
2019-09-27 14:57:43 -0300

    Adicionando migrations para ExternalEntity

--- app/models/support/common/city.rb
+++ app/models/support/common/city.rb
@@ -2,8 +2,11 @@ require_dependency 'support/application_record'
 
 module Support
   module Common
-    class City < ApplicationRecord
-      self.table_name = 'commonx.cities'
+    class City < ActiveRecord::Base
+      #self.table_name = 'commonx.cities'
+      self.table_name = 'extranet.address_cities'
+
+      scope :federal_district, -> { where(state_id: 7) }
     end
   end
 end
---
+++ db/migrate/entity/20190927170805_create_support_entity_accreditation_cooperative_units.rb
@@ -0,0 +1,15 @@
+class CreateSupportEntityAccreditationCooperativeUnits < ActiveRecord::Migration[5.2]
+  def change
+    create_table 'sihabx.entity_accreditation_cooperative_units' do |t|
+      t.integer :cooperative_mirror_id
+      t.integer :city_id
+      t.string  :cep
+      t.string  :address
+      t.string  :unit
+      t.string  :complement
+      t.boolean :head_office, boolean: false
+      
+      t.timestamps
+    end
+  end
+end
